283af4a018f5e103a7067e2bc8d87d990914cf28
Annie-815 <76701699+Annie-815@users.noreply.github.com>
2025-05-30 16:21:30 +0300

    tictactoe game in python

---
+++ TIC-TAC-TOE/player.py
@@ -0,0 +1,38 @@
+import math
+import random
+
+print("Welcome to TICTACTOE game.")
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+  
+class computerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+    
+
+class humanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(f"{self.letter}'s turn. Input move (0-8): ")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again. ")
+        return val   
+                    
